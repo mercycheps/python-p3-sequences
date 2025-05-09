3889040097fc6453f9da744b4661f3d68ebdea20
Mercy <maritimmercy8@gmail.com>
2025-05-09 06:58:06 +0300

    added range & length

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-   
-   fibonacci_sequence = []
-   
-   if length <= 0:
-       print("[]")
-       return
-   
-   a, b = 0, 1
-   
-   for _ in range(length):
-    fibonacci_sequence.append(a)
-    a, b = b, a + b
-       
-    print(fibonacci_sequence)
+    fibonacci_sequence = []
+
+    if length <= 0:
+        print([])
+        return
+
+    a, b = 0, 1
+
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+
+    print(fibonacci_sequence)  
